205fc025306677add08db8c108dcd1a351741e1f
Bogna Konstanty <rosiakb@gmail.com>
2017-07-17 14:41:10 +0200

    correction-4

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const webpack = require('webpack');
-////console.log('NODE_ENV:', env);
-//const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
+////console.log('NODE_ENV:', env);
+//const path = require('path');
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
+
 
 module.exports = {
     entry: './src/index.js',
@@ -39,6 +60,9 @@ module.exports = {
    }),
         new webpack.optimize.UglifyJsPlugin(),
         new OptimizeJsPlugin({sourceMap: false})
+        
     ]
+
+   
 
 };
